870d69894c474144c68432dbd0578466bfbc40c8
Anik Das <46628095+anikdas00@users.noreply.github.com>
2024-03-05 19:45:25 +0530

    RMX1901: Pixel experience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/genesis_RMX1901.mk
+    $(LOCAL_DIR)/aosp_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    genesis_RMX1901-userdebug \
-    genesis_RMX1901-eng
+    aosp_RMX1901-userdebug \
+    aosp_RMX1901-eng
